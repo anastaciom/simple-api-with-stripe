8c77cd16cfa3e805c0c5e32f7592a6471532d880
anastaciom <anastaciomenezes@Outlook.com>
2023-07-28 21:07:59 -0300

    feat: create route to return all plans

--- src/routes/subscription/index.ts
+++ src/routes/subscription/index.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from "express";
+import { stripe } from "../../services/stripe";
 
 const subscriptionRoutes = Router();
 
 subscriptionRoutes.get("/plans", async (req: Request, res: Response) => {
-  res.send({ message: "OK" });
+  const prices = await stripe.prices.list({
+    apiKey: process.env.SECRET_KEY_STRIPE,
+  });
+
+  return res.json(prices);
 });
 
 export { subscriptionRoutes };
---
+++ src/services/stripe.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.SECRET_KEY_STRIPE as string, {
+  apiVersion: "2022-11-15",
+});
+
+export { stripe };
