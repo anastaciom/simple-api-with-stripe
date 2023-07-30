d6464628b907e17ead89e7905dfa1fa6fdfe0029
anastaciom <anastaciomenezes@Outlook.com>
2023-07-30 16:42:49 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
+app.use(cors({ origin: process.env.ORIGIN }));
 app.use(express.json());
 app.use("/api", routes);
 
